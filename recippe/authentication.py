f6f83ee702fe7774c255ea8b0ad251b06c376d98
choeyoha <yoha6865@naver.com>
2022-11-05 22:27:06 +0900

    221105

---
+++ recippe/authentication.py
@@ -0,0 +1,26 @@
+from rest_framework.generics import get_object_or_404
+
+from .models import User
+
+from .serializers import UserInfoSerializer
+
+class ControlLogin_b():
+
+    def checkLogin(self, id, pw):
+        dbCheck = get_object_or_404(User, uid=id)
+        print("hello", dbCheck.nickname)
+
+        if dbCheck.password == pw:
+            print("Login Success")
+            serializer = self.sendResult(1, dbCheck)
+            return serializer
+        else:
+            print("Login Fail")
+            self.sendResult(0, self.serializer)
+
+    def sendResult(self, result, userInfo=None):
+        if result == 1:
+            serializer = UserInfoSerializer(userInfo)
+            return serializer
+        elif result == 0:
+            return "error"
